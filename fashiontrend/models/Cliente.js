a6ec899fae121c2e63303cd55ba188cd8a1d6ac7
Bárbara Riveros <barbara.riveros@usm.cl>
2024-05-13 14:56:32 -0400

    Hash y admins

--- fashiontrend/models/Cliente.js
+++ fashiontrend/models/Cliente.js
@@ -1,6 +1,4 @@
 const { Schema, model, models} = require("mongoose");
-import jwt from "jsonwebtoken";
-
 
 const ClienteSchema = new Schema({
     nombre: String,
--- fashiontrend/src/app/api/admins/route.js
+++ fashiontrend/src/app/api/admins/route.js
@@ -1,29 +1,28 @@
 import { initMongoose } from "@lib/mongoose";
 import { NextResponse } from "next/server";
-import Cliente from "@models/Cliente";
+import Admin from "@models/Admin";
 import bcryptjs from "bcrypt";
 
 
 export async function GET(){
     initMongoose();
-    const clients = await Cliente.find();
-    return NextResponse.json(clients)
+    const admins = await Admin.find();
+    return NextResponse.json(admins)
 }
-
+ 
 export async function POST(request) {
     initMongoose();
     try {
       const body = await request.json();
-      const newCliente = new Cliente(body);
+      const newAdmin = new Admin(body);
 
       const salt = await bcryptjs.genSalt(10)
-      const hashedPassword = await bcryptjs.hash(newCliente.password, salt)
-      newCliente.password = hashedPassword
+      const hashedPassword = await bcryptjs.hash(newAdmin.password, salt)
+      newAdmin.password = hashedPassword
       
-      const savedCliente = await newCliente.save();
+      const savedAdmin = await newAdmin.save();
 
-      console.log("Cliente guardado:", savedCliente);
-      return NextResponse.json(savedCliente);
+      return NextResponse.json(savedAdmin);
     } catch (error) {
       return NextResponse.json(error.message, {
         status: 400,
