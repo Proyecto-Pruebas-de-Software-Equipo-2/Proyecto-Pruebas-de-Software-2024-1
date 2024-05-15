3e63cf08f7530f638feb015eb231c021af186841
Vicenzo Brante <vicenzo.brante@usm.cl>
2024-05-15 16:49:34 -0400

    Boton de listado

--- fashiontrend/src/pages/_app.js
+++ fashiontrend/src/pages/_app.js
@@ -15,20 +15,20 @@ export default function App({ Component, pageProps }) {
   }
 
   return(
-    <div>
-        <nav className="flex justify-between items-center bg-gradient-to-b p-2 from-slate-800 to-transparent  mx-auto">
+    <div className="">
+        <nav className="flex w-screen justify-between items-center bg-gradient-to-b p-2 from-slate-800 to-transparent  mx-auto">
             <Link href={{pathname:"/"}}>
                 Fashion Trend
             </Link>
-            <form onSubmit={handleSubmit} className="w-max">
-              <input onChange={(e) => setNombre(e.target.value)} value={nombre} className="bg-gray-50 border w-max border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Buscar producto"/>
+            <form onSubmit={handleSubmit} className="px-8 items-center w-10/12">
+              <input onChange={(e) => setNombre(e.target.value)} value={nombre} className="bg-gray-50 border w-100 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Buscar producto"/>
             </form>
-              
+            
+            <Link className="px-6" href={{pathname:"/producto/listado"}}>Listado</Link>
 
             
-            <div className="flex items-center gap-6">
-                <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">Iniciar Sesión</button>
-                <ion-icon onclick="onToggleMenu(this)" name="menu" className="text-3xl cursor-pointer md:hidden"></ion-icon>
+            <div className="flex items-center">
+                <button className="bg-[#a6c1ee] text-white px-3 py-1 rounded-full hover:bg-[#87acec]">Iniciar Sesión</button>
             </div>
             </nav>
 
--- fashiontrend/src/pages/producto/listado.js
+++ fashiontrend/src/pages/producto/listado.js
@@ -1,11 +1,11 @@
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import ListadoComponent from "../components/Producto/listado";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Listado() {
     const [products, setproducts] = useState([]);
-
     useEffect(() => {
         fetch('http://localhost:3000/api/products')
         .then((response) => response.json())
@@ -15,16 +15,21 @@ export default function Listado() {
     const categorias = [...new Set(products.map((p)=>p.categoria))]
 
     return(
-        <div className="grid gap-4 grid-flow-cols-2">
-            {categorias.map((categorias) => (
-                <div>
-                    {products.filter(p => p.categoria == categorias).map(products => (
-                        <div>
-                            <ListadoComponent {...products}/>
-                        </div>
-                    ))}
-                </div>
-            ))}
+        <div>
+            <div className=" right-0 py-10 px-10">
+                <Link href={'/producto/crear'} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear Producto</Link>
+            </div>
+            <div className="grid gap-4 grid-flow-cols-2">
+                {categorias.map((categorias) => (
+                    <div>
+                        {products.filter(p => p.categoria == categorias).map(products => (
+                            <div>
+                                <ListadoComponent {...products}/>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
 
     )
